9dab18a6c5e3caadfd0b25970ed2093388df3207
unknown <a.hutt@lancaster.ac.uk>
2017-08-11 14:33:42 +0100

    Fixed a few bugs in CROPS.py.

--- python/CROPS(NF).py
+++ python/CROPS(NF).py
@@ -1,16 +1,18 @@
 from statistics import mean
-from pandas import concat
 from numpy import cumsum
 from functions import paste0
+from class_input import class_input
+from range_of_penalties import range_of_penalties
+from numpy import vstack
 
 def CROPS(data, pen_value, minseglen, shape, func, penalty = "CROPS", method = "PELT", test_stat = "Normal", Class = True, param_est = True):
     if method != "PELT":
         print('CROPS is a valid penalty choice only if method="PELT", please change your method or your penalty.')
     mu = mean(data)
-    sumstat = vstack([0, cumsum(coredata(data))],[0, cumsum(coredata(data) ** 2), cumsum([0, ((coredata(data) - mu) ** 2)])]).T
+    sumstat = vstack([0, cumsum(data)],[0, cumsum(data ** 2), cumsum([0, ((data - mu) ** 2)])]).T
     
     if test_stat == "Normal":
-        stat == "norm"
+        stat = "norm"
     elif test_stat == "Exponential":
         stat == "exp"
     elif test_stat == "Gamma":
@@ -32,7 +34,7 @@ def CROPS(data, pen_value, minseglen, shape, func, penalty = "CROPS", method = "
     if Class == True:
         ans = class_input(data = data, cpttype = cpttype, method = "PELT", test_stat = test_stat, penalty = penalty, pen_value = pen_value, minseglen = minseglen, param_estimates = param_est, out = out, shape = shape)
         if func == "var":
-            param_est(ans) = [param_est(ans), mean == mu]
+            ans.param_est = [ans.param_est, mean == mu]
         return(ans)
     else:
         return(out)

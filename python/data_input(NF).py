b802358b9dccf7c8fdea96ef77b8a08eded92a88
unknown <a.hutt@lancaster.ac.uk>
2017-08-15 14:14:39 +0100

    Fixed bug in data_input.py.

--- python/data_input(NF).py
+++ python/data_input(NF).py
@@ -1,22 +1,22 @@
-from numpy import mean
-from numpy import vstack
-from numpy import cumsum
-from numpy import square
-from numpy import subtract
-from BinSeg_one_func_minseglen import BINSEG
-from PELT_one_func_minseglen import PELT
-from SegNeigh_one_func_minseglen import SEGNEIGH
+#from numpy import mean
+#from numpy import vstack
+#from numpy import cumsum
+#from numpy import square
+#from numpy import subtract
+from PELT import PELT_meanvar_norm
+from BinSeg import binseg_meanvar_norm
+from multiple_norm import segneigh_meanvar_norm
 
 def data_input(data, method, pen_value, costfunc, minseglen, Q, var=0, shape=1):
-    if var != 0:
-        mu = var
-    else:
-        mu = mean(data)
-    sumstat = vstack([0,cumsum(data)] ,[0,cumsum(square(data))], [cumsum((0,square(subtract(data,mu))))]).T
+    #if var != 0:
+        #mu = var
+    #else:
+        #mu = mean(data)
+    #sumstat = vstack([0,cumsum(data)] ,[0,cumsum(square(data))], [cumsum((0,square(subtract(data,mu))))]).T
     if method == "PELT":
-        out = PELT(sumstat, pen = pen_value, cost_func = costfunc, minseglen = minseglen, shape = shape)
+        out = PELT_meanvar_norm(data, pen_value)
     elif method == "BinSeg":
-        out = BINSEG(sumstat, pen = pen_value, cost_func = costfunc, minseglen = minseglen, Q = Q, shape = shape)
+        out = binseg_meanvar_norm(data, Q, pen_value)
     elif method == "SegNeigh":
-        out = SEGNEIGH(data = data, pen_value = pen_value, Q = Q, costfunc = costfunc, var = var, shape = shape)
+        out = segneigh_meanvar_norm(data, Q, pen_value)
     return(out)

0b42af445c9a20ba9656e17cb4ec7e40c502aec0
unknown <a.hutt@lancaster.ac.uk>
2017-08-16 11:36:02 +0100

    Fixed bugs.

--- python/range_of_penalties.py
+++ python/range_of_penalties.py
@@ -1,52 +1,51 @@
 from math import log
 from numpy import size
-from numpy import array
 from functions import paste
 from collections import OrderedDict
 from numpy import multiply
 from functions import which_min
-from functions import is_equal
 from numpy import repeat
 from numpy import vstack
+from numpy import divide
+from PELT import PELT_meanvar_norm
+from numpy import subtract
+from numpy import add
+from numpy import power
+from numpy import abs
 
 def range_of_penalties(sumstat, minseglen, cost = "mean_norm", PELT = True, shape = 1):
-    min_pen = log(size((sumstat)/3) - 1)
-    max_pen = 10 * log(size((sumstat)/3) - 1)
-    NCALC = 0
+    min_pen = log(divide(size(sumstat),3) - 1)
+    max_pen = 10 * log(divide(size(sumstat),3) - 1)
     pen_interval = [min_pen, max_pen]
-    n = (size(sumstat)/3) - 1
+    n = (divide(size(sumstat),3)) - 1
     
     test_penalties = None
     numberofchangepoints = None
     penal = None
-    overall_cost = array()
     segmentations = None
-    b_between = array()
-    
-    # want to store and use Func, M and CP in PELT
     
     count = 0
     
     while size(pen_interval) > 0:
-        new_numcpts = array()
-        new_penalty = array()
-        new_cpts = array()
+        new_numcpts = []
+        new_penalty = []
+        new_cpts = []
         
         for b in range(1, size(pen_interval)):
             
-            ans = PELT(sumstat, pen = pen_interval[b], costfunc = cost, shape = shape, minseglen = minseglen)
+            ans = PELT_meanvar_norm(sumstat)
             resultingcpts = ans[[1]]
             new_numcpts[b] = size(resultingcpts)
             new_cpts[b] = list(resultingcpts[-size(resultingcpts)])
-            new_penalty[b] = ans[[2]][n] - ((ans[[3]][n] - 1) * pen_interval[b])
+            new_penalty[b] = ans[[2]][n] - (multiply((subtract(ans[[3]][n],1)),pen_interval[b]))
         
         if count == 0:
-            print(paste("Maximum number of runs of algorithm = ", new_numcpts[1] - new_numcpts[2] + 2, sep = ""))
+            print(paste("Maximum number of runs of algorithm = ", add(subtract(new_numcpts[1],new_numcpts[2]),2)))
             count = count + size(new_numcpts)
-            print(paste("Completed runs = ", count, sep = ""))
+            print(paste("Completed runs = ", count))
         else:
             count = count + size(new_numcpts)
-            print(paste("Completed runs = ", count, sep = ""))
+            print(paste("Completed runs = ", count))
         
         #Add the values calculated to the already stored values
         test_penalties = list(OrderedDict.fromkeys(sorted([test_penalties, pen_interval])))
@@ -57,7 +56,7 @@ def range_of_penalties(sumstat, minseglen, cost = "mean_norm", PELT = True, shap
         numberofchangepoints = multiply(sorted(multiply(new_numcpts, -1)), -1) #can use sorted to re-order
         penal = sorted(new_penalty)
         
-        ls = array()
+        ls = []
         
         for l in range(1, size(new_cpts)):
             ls[l] = size(new_cpts[[l-1]])
@@ -71,15 +70,15 @@ def range_of_penalties(sumstat, minseglen, cost = "mean_norm", PELT = True, shap
         tmppen_interval = None
         
         for i in range(1,size(test_penalties) - 1):
-            if abs(numberofchangepoints[i] - numberofchangepoints[i+1]) > 1: #only need to add a beta if difference in cpts>1
+            if abs(subtract(numberofchangepoints[i],numberofchangepoints[i+1])) > 1: #only need to add a beta if difference in cpts>1
                 j = i + 1
-                tmppen_interval = (penal[j] - penal[i]) * (((numberofchangepoints[i]) - (numberofchangepoints[j])) ** (-1))
+                tmppen_interval = multiply(subtract(penal[j],penal[i]),(power(subtract(numberofchangepoints[i],numberofchangepoints[j]),(-1))))
                 pen_interval = [pen_interval, tmppen_interval]
         
         if size(pen_interval) > 0:
             for k in range(size(pen_interval), 1):
-                index = which_min(abs(pen_interval[k] - test_penalties))
-                if is_equal(pen_interval[k], test_penalties[index]):
+                index = which_min(abs(subtract(pen_interval[k],test_penalties)))
+                if pen_interval[k] == test_penalties[index]:
                     pen_interval = pen_interval[-k]
     
     #prune values with same num_cp
@@ -102,7 +101,7 @@ def range_of_penalties(sumstat, minseglen, cost = "mean_norm", PELT = True, shap
         if k == nb:
             beta_e[k] = test_penalties[k]
         else:
-            beta_e[k] = (penal[k] - penal[k +1 ])/(numberofchangepoints[k + 1] - numberofchangepoints[k])
+            beta_e[k] = divide(subtract(penal[k],penal[k+1]),(subtract(numberofchangepoints[k+1],numberofchangepoints[k])))
     
     return(list(cpt_out = vstack(beta_interval = beta_int, numberofchangepoints = numberofchangepoints, penalised_cost = penal), changepoints = segmentations))
     #segmentations is output matrix

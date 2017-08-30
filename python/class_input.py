a5ebdf87407d697aeb71a814865175ba9d44df38
ahutt <a.hutt@lancaster.ac.uk>
2017-08-30 17:38:34 +0100

    Started working on documentation.

--- python/class_input.py
+++ python/class_input.py
@@ -6,6 +6,33 @@ from numpy import diff
 from param import param
 
 def class_input(data, cpttype, method, test_stat, penalty, pen_value, minseglen, param_estimates, out = list(), Q = None, shape = None):
+    """
+    class_input(data, cpttype, method, test_stat, penalty, pen_value, minseglen, param_estimates, out = list(), Q = None, shape = None)
+    
+    This function helps to input all the necessary information into the correct format for cpt and cpt_range classes.
+
+    This function is called by cpt_mean, cpt_var and cpt_meanvar when class=TRUE. This is not intended for use by regular users of the package. It is exported for developers to call directly for speed and convenience.
+
+    WARNING: No checks on arguments are performed!
+    
+    Parameters
+    ----------
+    data : A vector, ts object or matrix containing the data within which you wish to find a changepoint. If data is a matrix, each row is considered a separate dataset.
+    cpttype : Type of changepoint analysis performed as a text string, e.g. "Mean", "Mean and Variance".
+    method : Choice of "AMOC", "PELT", "SegNeigh" or "BinSeg".
+    test_stat : The assumed test statistic / distribution of the data.  Currently only "Normal" and "CUSUM" supported.
+    penalty : Choice of "None", "SIC", "BIC", "MBIC", AIC", "Hannan-Quinn", "Asymptotic", "Manual" and "CROPS" penalties.  If Manual is specified, the manual penalty is contained in the pen.value parameter. If Asymptotic is specified, the theoretical type I error is contained in the pen.value parameter.  If CROPS is specified, the penalty range is contained in the pen.value parameter; note this is a vector of length 2 which contains the minimum and maximum penalty value.  Note CROPS can only be used if the method is "PELT". The predefined penalties listed DO count the changepoint as a parameter, postfix a 0 e.g."SIC0" to NOT count the changepoint as a parameter.
+    pen_value : Numerical penalty value used in the analysis (positive).
+    minseglen : Minimum segment length used in the analysis (positive integer).
+    param_estimates : Logical. If True then parameter estimates are calculated. If False no parameter estimates are calculated and the slot is blank in the returned object.
+    out : List of output from BinSeg, PELT or other method used. Function assumes that method and format of out match.
+    Q : The value of Q used in the BinSeg or SegNeigh methods.
+    shape : Value of the assumed known shape parameter required when test_stat="Gamma".
+    
+    Returns
+    -------
+    
+    """
     if method == "BinSeg" or method == "SegNeigh" or penalty == "CROPS":
         ans = "cpt_range".__new__
     else:

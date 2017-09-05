6bf552739eaad08dad8c3836351fac4ced84e2bf
ahutt <a.hutt@lancaster.ac.uk>
2017-09-05 11:20:04 +0100

    Fixed bugs.

--- python/decision.py
+++ python/decision.py
@@ -1,3 +1,5 @@
+from sys import exit
+from pandas import DataFrame
 from numpy import size
 
 def single_decision(tau, null, alt, n = 0, diffparam = 1, pen_value = 0):
@@ -36,18 +38,22 @@ def decision(tau, null, alt = None, penalty = "MBIC", n = 0, diffparam = 1, pen_
     """
     if alt == None:
         if size(tau) != size(null):
-            print("Lengths of tau and null do not match")
+            exit("Lengths of tau and null do not match")
     else:
         if (size(tau) != size(null)) or (size(tau) != size(alt)):
-            print('Lengths of tau, null and alt do not match')
+            exit("Lengths of tau, null and alt do not match")
     if size(tau) == 1:
         out = single_decision(tau, null, alt, n, diffparam, pen_value)
-        out.rename(columns = {'cpt'}, inplace = True)
-        return(list(cpt = out, pen = pen_value))
+#        out.columns = ['cpt']
+        cpt = out
+        pen = pen_value
+        return(list((cpt, pen)))
     else:
         rep = size(tau)
         out = None
-        for i in range(1,rep):
-            out[i] = single_decision(tau[i-1], null[i-1], alt[i-1], n, diffparam, pen_value)
-        out.rename(columns = {'cpt'}, inplace = True)
-        return(list(cpt = out, pen = pen_value))
+        for i in range(1,rep+1):
+            out[i-1] = single_decision(tau[i-1], null[i-1], alt[i-1], n, diffparam, pen_value)
+#        out.columns = ['cpt']
+        cpt = out
+        pen = pen_value
+        return(list((cpt, pen)))

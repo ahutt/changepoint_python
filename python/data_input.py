8e84e990043d8a32793279024d35e09f7c1ca6fd
ahutt <a.hutt@lancaster.ac.uk>
2017-09-05 10:12:19 +0100

    Fixed bug. Removed unused lines of code.

--- python/data_input.py
+++ python/data_input.py
@@ -1,25 +1,34 @@
-#from numpy import mean
-#from numpy import vstack
-#from numpy import cumsum
-#from numpy import square
-#from numpy import subtract
+from numpy import mean
 from PELT import PELT_meanvar_norm
 from BinSeg import binseg_meanvar_norm
 from SegNeigh import segneigh_meanvar_norm
+from numpy import subtract
+from numpy import size
+from functions import greater_than
+from functions import truefalse
 
 def data_input(data, method, pen_value, costfunc, minseglen, Q, var=0, shape=1):
     """
     PLEASE ENTER DETAILS.
     """
-    #if var != 0:
-        #mu = var
-    #else:
-        #mu = mean(data)
-    #sumstat = vstack([0,cumsum(data)] ,[0,cumsum(square(data))], [cumsum((0,square(subtract(data,mu))))]).T
+    if var != 0:
+        mu = var
+    else:
+        mu = mean(data)
     if method == "PELT":
         out = PELT_meanvar_norm(data, pen_value)
+        cpts = out[[1]]
     elif method == "BinSeg":
+        cpts = out[[1]]
         out = binseg_meanvar_norm(data, Q, pen_value)
+        if out.op_cpts == 0:
+            cpts = size(data)
+        else:
+            cpts = [sorted(out.cps[0,subtract(list(range(1,out.op_cpts + 1)),1)]),size(data)]
     elif method == "SegNeigh":
         out = segneigh_meanvar_norm(data, Q, pen_value)
+        if out.op_cpts == 0:
+            cpts = size(data)
+        else:
+            cpts = [sorted(out.op_cps[out.op_cpts,:][truefalse(subtract(out.cps[out.op_cpts,:],1),greater_than(subtract(out.cps[out.op_cpts,:],1),0))]),size(data)]
     return(out)

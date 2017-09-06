bf43571b1f252d87fecdb93623f89bc121a8daac
ahutt <a.hutt@lancaster.ac.uk>
2017-09-06 14:55:29 +0100

    Fixed bugs.

--- python/param.py
+++ python/param.py
@@ -5,6 +5,9 @@ from numpy import size
 from numpy import vstack
 from numpy import multiply
 from numpy import matrix
+from numpy import divide
+from numpy import square
+from sys import exit
 
 def param_mean(object):
     """
@@ -53,7 +56,7 @@ def param_trend(object):
     sumstat = vstack(cumsum([0, data]), cumsum([0, multiply(data,[range(1,n)])]))
     cptsumstat = matrix([sumstat[object.cpts + 1,:] - sumstat[[0,object.cpts] + 1,:]])
     cptsumstat[:,1] = cptsumstat[:,1] - multiply(cptsumstat[:,0],[0, object.cpts])
-    
+
     thetaS = (2 * cptsumstat[:,0] * (2 * seglen + 1) - 6 * cptsumstat[:,1])/(2 * seglen * (2 * seglen + 1) - 3 * seglen * (seglen + 1))
     thetaT = (6 * cptsumstat[:,1])/((seglen + 1) * (2 * seglen + 1)) + (thetaS * (1 - ((3 * seglen)/((2 * seglen) + 1))))
     return(vstack(thetaS,thetaT))
@@ -65,7 +68,7 @@ def param_meanar(object):
     seglen = object.seg_len
     data = object.data_set
     n = size(data) - 1
-    sumstat = vstack(cumsum([0, data[-1]]), cumsum(c[0, data[-(n+1)]]), cumsum(c[0,multiply(data[-1],data[-(n+1)])]), cumsum([0,square(data[-1])]), cumsum(c[0,square(data[-(n+1)])]))
+    sumstat = vstack(cumsum([0, data[-1]]), cumsum([0, data[-(n+1)]]), cumsum([0,multiply(data[-1],data[-(n+1)])]), cumsum([0,square(data[-1])]), cumsum([0,square(data[-(n+1)])]))
     cptsumstat = matrix(sumstat[object.cpts + 1,:] - sumstat[[0,object.cpts] + 1,:])
     beta2 = (2 * seglen * cptsumstat[:,3] - cptsumstat[:,1] * cptsumstat[:,2])/(2 * seglen * cptsumstat[:,5] * (1 - square(cptsumstat[:,2])))
     beta1 = (2 * cptsumstat[:,1] - beta2 * cptsumstat[:,2])/(2 * seglen)
@@ -92,50 +95,50 @@ def param_trender(object):
 def param(object):
     """
     param(object)
-    
+
     Generic function that returns parameter estimates.
-    
+
     Parameters
     ----------
     object : Depending on the class of object depends on the method used to find the parameter estimates (and if one exists).
-    
+
     Returns
     -------
     PLEASE ENTER DETAILS.
     """
-    if object.cpttype == "mean":
-        object.param_est = list(mean = param_mean(object))
-    elif object.cpttype == "variance":
-        object.param_est = list(variance = param_var(object))
-    elif object.cpttype == "mean and variance":
-        if object.test_stat == "Normal":
-            object.param_est = list(mean = param_mean(object), variance = param_var(object))
-        elif object.test_stat == "Gamma":
-            object.param_est = list(scale = param_scale(object, shape = shape), shape = shape)
-        elif object.test_stat == "Exponential":
-            object.param_est = list(rate = 1/param_mean(object))
-        elif object.test_stat == "Poisson":
-            object.param_est == list(Lambda = param_mean(object))
+    if object[1] == "mean":
+        object[11] = list(param_mean(object))
+    elif object[1] == "variance":
+        object[11] = list(param_var(object))
+    elif object[1] == "mean and variance":
+        if object[3] == "Normal":
+            object[11] = list(param_mean(object), param_var(object))
+        elif object[3] == "Gamma":
+            object[11] = list(param_scale(object, shape), shape)
+        elif object[3] == "Exponential":
+            object[11] = list(divide(1,param_mean(object)))
+        elif object[3] == "Poisson":
+            object[11] == list(param_mean(object))
         else:
-            print("Unknown test statistic for a change in mean and variance")
-    elif object.cpttype == "trend":
-        if object.test_stat == "Normal":
+            exit("Unknown test statistic for a change in mean and variance")
+    elif object[1] == "trend":
+        if object[3] == "Normal":
             tmp = param_trend(object)
-            object.param_est = list(thetaS = tmp[:,0], thetaT = tmp[:,1])
+            object[11] = list(tmp[:,0], tmp[:,1])
         else:
-            print("Unknown test statistic for a change in trend")
-    elif object.cpttype == "trender":
-        if object.test_stat == "Normal":
+            exit("Unknown test statistic for a change in trend")
+    elif object[1] == "trender":
+        if object[3] == "Normal":
             tmp = param_trender(object)
-            object.param_est = list(beta = tmp[:,0], thetajpo = tmp[:,1], thetaj = tmp[:,2])
+            object[11] = list(beta = tmp[:,0], thetajpo = tmp[:,1], thetaj = tmp[:,2])
         else:
-            print("Unknown test statistic for a change in trend+ar")
-    elif object.cpttype == "meanar":
-        if object.test_stat == "Normal":
+            exit("Unknown test statistic for a change in trend+ar")
+    elif object[1] == "meanar":
+        if object[3] == "Normal":
             tmp = param(object)
-            object.param_est = list(beta1 = tmp[:,0], beta2 = tmp[:,1])
+            object[11] = list(tmp[:,0], tmp[:,1])
         else:
-            print("Unknown test statistic for a change in mean+ar")
+            exit("Unknown test statistic for a change in mean+ar")
     else:
-        print("Unknown changepoint type, must be 'mean', 'variance', 'mean and variance', 'trend', 'meanar' or 'trendar'.")
+        exit("Unknown changepoint type, must be 'mean', 'variance', 'mean and variance', 'trend', 'meanar' or 'trendar'.")
     return(object)

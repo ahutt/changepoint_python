76ae1952bee36b8ee2912642cc3a1c0e9e54bd06
Robin Long <r.long@lancs.ac.uk>
2017-12-07 11:02:22 +0000

    Corrected name in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='changepoint',
       version='0.1',
       description='Python version of R-changepoint package',
       url='https://github.com/ahutt/changepoint_python',

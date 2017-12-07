6a9999093e3a793ce06fb3e3045f86e37344d0a2
Robin Long <r.long@lancs.ac.uk>
2017-12-07 11:19:11 +0000

    Corrected __init__.py to import all modules individually

--- changepoint/__init__.py
+++ changepoint/__init__.py
@@ -1,1 +1,23 @@
-from . import *
+from . import BinSeg
+from . import CROPS
+from . import PELT
+from . import SegNeigh
+from . import class_input
+from . import cpt
+from . import data_input
+from . import decision
+from . import exp
+from . import functions
+from . import gamma
+from . import multiple_nonparametric
+from . import multiple_norm
+from . import param_cpt
+from . import param_cpt_range
+from . import penalty_decision
+from . import poisson
+from . import range_of_penalties
+from . import single_nonparametric
+from . import single_norm
+
+
+
